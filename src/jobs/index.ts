afe7df8dc0d4d73d5b2cf93c7e92d502005bc681
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2024-07-31 12:12:09 -0300

    feat: create general export for jobs

---
+++ src/jobs/index.ts
@@ -0,0 +1,4 @@
+export { default as Signup } from "./signup/index";
+export { default as SignupRetry } from "./signup/retry";
+export { default as WelcomeMail } from "./welcome-mail/index";
+export { default as WelcomeMailRetry } from "./welcome-mail/retry";
